1a5f7b872e48b1a7a950ba39935c5652e7d54cb7
Jefgit <Riazondajf@gmail.com>
2024-07-10 11:11:05 +0800

    added filtering of notes

--- Redux/redux-note/src/App.jsx
+++ Redux/redux-note/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
+import VisibilityFilter from './components/VisibilityFilter'
 
 
 function App() {
-
+  
   return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
--- Redux/redux-note/src/main.jsx
+++ Redux/redux-note/src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {createStore} from 'redux'
+import {createStore, combineReducers} from 'redux'
 import { Provider } from 'react-redux'
+import { createNote } from './reducers/noteReducer.js'
+import { filterChange } from './reducers/filterReducer.js'
 
 import App from './App.jsx'
 import noteReducer from './reducers/noteReducer.js'
+import filterReducer from './reducers/filterReducer.js'
 
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+})
 
-const store = createStore(noteReducer)
+const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
