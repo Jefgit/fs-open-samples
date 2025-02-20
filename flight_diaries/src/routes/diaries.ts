f07ac53348d8cf2be2fd22c1a5e13a2971bb852a
Jefgit <Riazondajf@gmail.com>
2025-02-20 18:58:43 +0800

    Typing an Express app

---
+++ flight_diaries/src/routes/diaries.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import { Response } from 'express';
+import { NonSensitiveDiaryEntry } from '../types';
+import diaryService from '../services/diaryService';
+import toNewDiaryEntry from '../utils';
+import { z } from 'zod';
+
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+
+    const diary = diaryService.findById(Number(req.params.id));
+
+    if (diary) {
+        res.send(diary);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+router.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
+    res.send(diaryService.getNonSensitiveEntries());
+});
+
+router.post('/', (req, res) => {
+
+    try {
+        const newDiaryEntry = toNewDiaryEntry(req.body);
+
+        const addedEntry = diaryService.addDiary(newDiaryEntry);
+        res.json(addedEntry);
+    } catch (error: unknown) {
+
+        if (error instanceof z.ZodError) {
+            res.status(400).send({ error: error.issues });
+        } else {
+            res.status(400).send({ error: 'unknown error' });
+        }
+    }
+
+});
+
+router.post('/', (_req, res) => {
+
+    res.send('Saving a diary!');
+});
+
+export default router;
---
+++ flight_diaries/src/types.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+import { newEntrySchema } from './utils';
+
+export enum Weather {
+    Sunny = 'sunny',
+    Rainy = 'rainy',
+    Cloudy = 'cloudy',
+    Windy = 'windy',
+    Stormy = 'stormy'
+};
+
+export enum Visibility {
+    Great = 'great',
+    Good = 'good',
+    Ok = 'ok',
+    Poor = 'poor',
+};
+
+
+export type NewDiaryEntry = z.infer<typeof newEntrySchema>;
+export interface DiaryEntry extends NewDiaryEntry {
+    id: number;
+}
+export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
+
